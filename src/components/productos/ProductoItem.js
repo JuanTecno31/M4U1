904befaa38da8f270372732a0580d604f6cc1a9e
JuanTecno31 <juanarrizubieta@hotmail.com>
2022-12-01 20:19:42 -0300

    heroku 8

--- src/components/productos/ProductoItem.js
+++ src/components/productos/ProductoItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '../../styles/components/pages/ProductosPage.css';
+// import '../../styles/components/pages/ProductosPage.css';
 
 const ProductoItem = (props) => {
 
